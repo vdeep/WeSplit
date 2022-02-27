8da78d775dd9508260f72fd965601d2b9f80b131
Vishal Kanojia <vishal.deep@live.com>
2022-02-27 20:02:23 +0530

    Challenge 3 complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
 
-    let tipPercentages = [10, 15, 20, 25, 0]
-
     var totalPayableAmount: Double {
         let tipSelection = Double(tipPercentage)
 
@@ -46,11 +44,10 @@ struct ContentView: View {
 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0...100, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
